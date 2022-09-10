3100db4641c03356e1fed58fdabd06fba14c2d3f
Kevin Price <collegekevin@gmail.com>
2022-09-09 18:47:31 -0700

    Added possibly final facts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Software Engineering student at Flatiron School";
+h2.textContent = "Software Engineering Student at Flatiron School";
 
 document.querySelector("body").appendChild(h2);
